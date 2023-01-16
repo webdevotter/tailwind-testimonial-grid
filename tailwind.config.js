9db7ebd365020e8a43adf36aa0c00ed903b76e90
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-16 12:42:59 -0600

    First Box

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px'
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serid'],
+      }
+    },
   },
   plugins: [],
 }
